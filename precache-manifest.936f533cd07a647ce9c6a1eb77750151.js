f4acc9608f4384d72252f108faa8f0a33288129c
unknown <viktoriya.simakova@gmail.com>
2018-10-26 23:19:40 +0300

    Updates

---
+++ precache-manifest.936f533cd07a647ce9c6a1eb77750151.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee75b0acb409554d3b30b5977694ed34",
+    "url": "/Ms-Test/static/media/footer-logo.ee75b0ac.svg"
+  },
+  {
+    "revision": "34ae6733bdc3ec27e8a1",
+    "url": "/Ms-Test/static/css/main.cde373cc.chunk.css"
+  },
+  {
+    "revision": "261d8d98f040a5028c99",
+    "url": "/Ms-Test/static/js/1.261d8d98.chunk.js"
+  },
+  {
+    "revision": "72993f8684522d913fb5",
+    "url": "/Ms-Test/static/js/runtime~main.72993f86.js"
+  },
+  {
+    "revision": "0cfbbb0729e32a39affaf28cc1e7e888",
+    "url": "/Ms-Test/static/media/photo.0cfbbb07.jpg"
+  },
+  {
+    "revision": "34ae6733bdc3ec27e8a1",
+    "url": "/Ms-Test/static/js/main.34ae6733.chunk.js"
+  },
+  {
+    "revision": "443c6f6f73180345528ba42e59ed513d",
+    "url": "/Ms-Test/static/media/Avenir-Light.443c6f6f.woff"
+  },
+  {
+    "revision": "94176ea273c95d59a0bd29010180f452",
+    "url": "/Ms-Test/static/media/Avenir-Light.94176ea2.woff2"
+  },
+  {
+    "revision": "c71bc76fc334a751dcadb144f43a41f2",
+    "url": "/Ms-Test/static/media/Avenir-Roman.c71bc76f.woff2"
+  },
+  {
+    "revision": "f4aa35b27e6731c9406f14b8d8498013",
+    "url": "/Ms-Test/static/media/Avenir-Roman.f4aa35b2.woff"
+  },
+  {
+    "revision": "9a5595cbe7f0103590068680f9cb7fe5",
+    "url": "/Ms-Test/index.html"
+  }
+];
