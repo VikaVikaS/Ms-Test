b6fd2a5ea185958ea85534112492f59c40ac55ef
Viktoriia Simakova <viktoriia.simakova@tbwamobile.com>
2019-06-20 23:00:39 +0300

    Test video

---
+++ build/precache-manifest.aa99672d216624fba9051ee7066bb5b6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ba4212e8b99a45ee663738d26ff20ab",
+    "url": "/Ms-Test/static/media/footer-logo.5ba4212e.svg"
+  },
+  {
+    "revision": "b6e4048494347066a778",
+    "url": "/Ms-Test/static/css/main.7abe39c6.chunk.css"
+  },
+  {
+    "revision": "261d8d98f040a5028c99",
+    "url": "/Ms-Test/static/js/1.261d8d98.chunk.js"
+  },
+  {
+    "revision": "72993f8684522d913fb5",
+    "url": "/Ms-Test/static/js/runtime~main.72993f86.js"
+  },
+  {
+    "revision": "b7600c3a9a9ded491ace4f9b775fee71",
+    "url": "/Ms-Test/static/media/photo.b7600c3a.jpg"
+  },
+  {
+    "revision": "b6e4048494347066a778",
+    "url": "/Ms-Test/static/js/main.b6e40484.chunk.js"
+  },
+  {
+    "revision": "94176ea273c95d59a0bd29010180f452",
+    "url": "/Ms-Test/static/media/Avenir-Light.94176ea2.woff2"
+  },
+  {
+    "revision": "c71bc76fc334a751dcadb144f43a41f2",
+    "url": "/Ms-Test/static/media/Avenir-Roman.c71bc76f.woff2"
+  },
+  {
+    "revision": "443c6f6f73180345528ba42e59ed513d",
+    "url": "/Ms-Test/static/media/Avenir-Light.443c6f6f.woff"
+  },
+  {
+    "revision": "f4aa35b27e6731c9406f14b8d8498013",
+    "url": "/Ms-Test/static/media/Avenir-Roman.f4aa35b2.woff"
+  },
+  {
+    "revision": "30d2d196c7c1c8303dc3553af6417afa",
+    "url": "/Ms-Test/index.html"
+  }
+];
